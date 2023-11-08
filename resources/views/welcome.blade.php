40120e1a920ce66ea8a44e91dc90a6471979a219
joshiabhijj <joshiabhishekje777@gmail.com>
2023-11-08 13:00:08 +0530

    new data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,8 @@
                                     </svg>
                                 </div>
  <div>my new data</div>
+ <div>not </div>
+
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
