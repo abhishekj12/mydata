f55a121842111e846907af4bcc574f3386f9f28f
“abhishekj12” <abhishek.xerarc@gmail.com>
2023-11-08 15:59:25 +0530

    third commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return 'abhishek';
+    return 'abhishekjjj';
     return 'abhi';
     return 'hi';
     return 'data';
