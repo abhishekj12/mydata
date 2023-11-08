0d3131be77600cd7fbaeb2c68b2041ae1a47e8cd
“abhishekj12” <abhishek.xerarc@gmail.com>
2023-11-08 12:21:07 +0530

    datat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,6 +38,7 @@
                     </svg>
                     <div></div>
                 </div>
+                <div></div>
 
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
