73352d1e83b63eb5acddf9305df37ff56276dd3c
joshiabhijj <joshiabhishekje777@gmail.com>
2023-11-08 16:04:53 +0530

    conflict solved

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return 'abhishekjjj';
+    return 'abhishekasdsdsagfdgadg';
     return 'abhi';
     return 'hi';
     return 'data';
